c1f879cd4b1f738048552ef66b803dc7cdae45e3
CNT2001 <127881418+it22172228@users.noreply.github.com>
2025-07-21 20:40:50 +0530

    Implemnted Vapi sdk & Header

--- lib/actions/companion.action.ts
+++ lib/actions/companion.action.ts
@@ -41,3 +41,17 @@ export const getAllCompanions = async ({limit = 10,page = 1,subject,topic} :GetA
     return companions;
 
 }
+
+
+export const getCompanion = async (id: string) => {
+    const supabase = createSupabaseClient();
+
+    const { data, error } = await supabase
+        .from('companions')
+        .select()
+        .eq('id', id);
+
+    if(error) return console.log(error);
+
+    return data[0];
+}
